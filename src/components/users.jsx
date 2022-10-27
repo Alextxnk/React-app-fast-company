a0ac2ece5756e378efec6ef3b13b5697251abfb9
Alextxnk <alexsolov766@gmail.com>
2022-10-28 01:18:01 +0300

    change component

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,42 +5,52 @@ import api from '../api';
 const Users = () => {
    const [users, setUsers] = useState(api.users.fetchAll());
    const handleDelete = userId => {
+      setUsers(users.filter((user) => user._id !== userId));
 
+      const renderPhrase = (number) => {
+         if (number > 4 && number < 15) {
+            return 'человек тусанет';
+         }
+
+         return 'человек тусанет';
+      };
    };
 
    return (
       <>
          <span className={'badge bg-' + (users.length > 0 ? 'primary' : 'danger')}>
-            Готовы с тобой тусануть
+            {users.length > 0 ? `${users.length} ${renderPhrase(users.length)} с тобой сегодня` : 'Никто с тобой не тусанет'}
          </span>
-         <table className='table'>
-            <thead>
-               <tr>
-                  <th scope="col">Имя</th>
-                  <th scope="col">Качества</th>
-                  <th scope="col">Профессия</th>
-                  <th scope="col">Встретился, раз</th>
-                  <th scope="col">Оценка</th>
-                  <th />
-               </tr>
-            </thead>
-            <tbody>
-               {users.map((user) => (
-                  <tr key={user._id}>
-                     <td>{user.name}</td>
-                     <td>{user.qualities.map(item => <span className={'badge m-1 bg-' + item.color} key={item._id}>{item.name}</span>)}</td>
-                     <td>{user.profession.name}</td>
-                     <td>{user.completedMeetings}</td>
-                     <td>{user.rate}</td>
-                     <td>
-                        <button className={'btn btn-danger'} onClick={() => handleDelete(user._id)}>
-                           Удалить
-                        </button>
-                     </td>
+         {users.length > 0 &&
+            <table className='table'>
+               <thead>
+                  <tr>
+                     <th scope="col">Имя</th>
+                     <th scope="col">Качества</th>
+                     <th scope="col">Профессия</th>
+                     <th scope="col">Встретился, раз</th>
+                     <th scope="col">Оценка</th>
+                     <th />
                   </tr>
-               ))}
-            </tbody>
-         </table>
+               </thead>
+               <tbody>
+                  {users.map((user) => (
+                     <tr key={user._id}>
+                        <td>{user.name}</td>
+                        <td>{user.qualities.map(item => <span className={'badge m-1 bg-' + item.color} key={item._id}>{item.name}</span>)}</td>
+                        <td>{user.profession.name}</td>
+                        <td>{user.completedMeetings}</td>
+                        <td>{user.rate}</td>
+                        <td>
+                           <button className={'btn btn-danger'} onClick={() => handleDelete(user._id)}>
+                              Удалить
+                           </button>
+                        </td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         }
       </>
    );
 };
