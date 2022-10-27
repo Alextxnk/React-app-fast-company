42b59b80b35afca0785af9e44c9a0b341bf61f49
Alextxnk <alexsolov766@gmail.com>
2022-10-28 00:31:37 +0300

    change component

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../api';
 
 
 const Users = () => {
-   return <h1>Users</h1>;
+   const [users, setUsers] = useState(api.users.fetchAll());
+   return (
+      <table className='table'>
+         <thead>
+            <tr>
+               <th scope="col">#</th>
+               <th scope="col">First</th>
+               <th scope="col">Last</th>
+               <th scope="col">Handle</th>
+            </tr>
+         </thead>
+         <tbody>
+            <tr>
+               <th scope="row">1</th>
+               <td>Mark</td>
+               <td>Otto</td>
+               <td>@mdo</td>
+            </tr>
+         </tbody>
+      </table>
+   );
 };
 
 export default Users;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Users from "./components/users";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Users from './components/users';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <Users />
