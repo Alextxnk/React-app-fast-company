d69cdade06a19e915fd6d7e67d73cb5f03422b71
Alextxnk <alexsolov766@gmail.com>
2022-10-28 00:55:31 +0300

    change component

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,25 +4,44 @@ import api from '../api';
 
 const Users = () => {
    const [users, setUsers] = useState(api.users.fetchAll());
+   const handleDelete = userId => {
+
+   };
+
    return (
-      <table className='table'>
-         <thead>
-            <tr>
-               <th scope="col">#</th>
-               <th scope="col">First</th>
-               <th scope="col">Last</th>
-               <th scope="col">Handle</th>
-            </tr>
-         </thead>
-         <tbody>
-            <tr>
-               <th scope="row">1</th>
-               <td>Mark</td>
-               <td>Otto</td>
-               <td>@mdo</td>
-            </tr>
-         </tbody>
-      </table>
+      <>
+         <span className={'badge bg-' + (users.length > 0 ? 'primary' : 'danger')}>
+            Готовы с тобой тусануть
+         </span>
+         <table className='table'>
+            <thead>
+               <tr>
+                  <th scope="col">Имя</th>
+                  <th scope="col">Качества</th>
+                  <th scope="col">Профессия</th>
+                  <th scope="col">Встретился, раз</th>
+                  <th scope="col">Оценка</th>
+                  <th />
+               </tr>
+            </thead>
+            <tbody>
+               {users.map((user) => (
+                  <tr key={user._id}>
+                     <td>{user.name}</td>
+                     <td>{user.qualities.map(item => <span className={'badge m-1 bg-' + item.color} key={item._id}>{item.name}</span>)}</td>
+                     <td>{user.profession.name}</td>
+                     <td>{user.completedMeetings}</td>
+                     <td>{user.rate}</td>
+                     <td>
+                        <button className={'btn btn-danger'} onClick={() => handleDelete(user._id)}>
+                           Удалить
+                        </button>
+                     </td>
+                  </tr>
+               ))}
+            </tbody>
+         </table>
+      </>
    );
 };
 
