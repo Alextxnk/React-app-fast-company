45eb114c59915e4245399e87deb8004f803a905a
Alextxnk <alexsolov766@gmail.com>
2022-10-28 12:02:36 +0300

    change component

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,12 +4,20 @@ import api from '../api';
 
 const Users = () => {
    const [users, setUsers] = useState(api.users.fetchAll());
+
    const handleDelete = userId => {
       setUsers(users.filter((user) => user._id !== userId));
    };
+
    const renderPhrase = (number) => {
+      const lastOne = Number(number.toString().slice(-1));
+
       if (number > 4 && number < 15) {
          return 'человек тусанет';
+      }
+
+      if ([2, 3, 4].indexOf(lastOne) >= 0) {
+         return 'человека тусанут';
       }
 
       return 'человек тусанет';
