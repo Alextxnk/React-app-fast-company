f268b1f1a968af7ed83df439abbfd25a207dd2ac
Alextxnk <alexsolov766@gmail.com>
2023-03-03 22:23:17 +0300

    add new components and change derictory

---
+++ src/app/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import UsersList from './components/usersList';
+import SearchStatus from './components/searchStatus';
+import api from './api';
+
+function App() {
+   const [users, setUsers] = useState(api.users.fetchAll());
+
+   const handleDelete = (userId) => {
+      setUsers(users.filter((user) => user._id !== userId));
+   };
+
+   const handleToggleBookMark = (id) => {
+      setUsers(
+         users.map((user) => {
+            if (user._id === id) {
+               return { ...user, bookmark: !user.bookmark };
+            }
+            return user;
+         })
+      );
+      console.log(id);
+   };
+
+   return (
+      <div>
+         <SearchStatus length={users.length} />
+         <UsersList onDelete={handleDelete} onToggleBookMark={handleToggleBookMark} users={users} />
+      </div>
+   );
+}
+
+export default App;
