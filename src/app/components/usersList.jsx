4e115abfe9144a13b88032a94ec94a9252a83df0
Alextxnk <alexsolov766@gmail.com>
2023-03-03 23:51:03 +0300

    add new components and change derictory

--- src/app/components/usersList.jsx
+++ src/app/components/usersList.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Pagination from './pagination';
 import User from './user';
 
 const UsersList = ({ users, ...rest }) => {
+   const count = users.length;
+   const pageSize = 4;
+   const [currentPage, setCurrenPage] = useState(1);
+
+   const handlePageChange = (pageIndex) => {
+      setCurrenPage(pageIndex);
+   };
+
    return (
       <>
-         {users.length > 0 &&
+         {count > 0 && (
             <table className='table'>
-               <thead>
-                  <tr>
-                     <th scope='col'>Имя</th>
-                     <th scope='col'>Качества</th>
-                     <th scope='col'>Профессия</th>
-                     <th scope='col'>Встретился, раз</th>
-                     <th scope='col'>Оценка</th>
-                     <th scope='col'>Избранное</th>
-                     <th />
-                  </tr>
-               </thead>
-               <tbody>
-                  {users.map((user) => (
-                     <User key={user._id} {...rest} {...user} />
-                  ))}
-               </tbody>
-            </table>
-         }
+            <thead>
+               <tr>
+                  <th scope='col'>Имя</th>
+                  <th scope='col'>Качества</th>
+                  <th scope='col'>Профессия</th>
+                  <th scope='col'>Встретился, раз</th>
+                  <th scope='col'>Оценка</th>
+                  <th scope='col'>Избранное</th>
+                  <th scope='col'>Действия</th>
+               </tr>
+            </thead>
+            <tbody>
+               {users.map((user) => (
+                  <User key={user._id} {...rest} {...user} />
+               ))}
+            </tbody>
+         </table>
+         )}
+         <Pagination itemsCount={count} pageSize={pageSize} currentPage={currentPage} onPageChange={handlePageChange} />
       </>
    );
 };
